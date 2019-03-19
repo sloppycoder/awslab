51473abeebbfe9de57628bedf0a2c5e21c7d6e1e
Li Lin <guru.lin@gmail.com>
2019-03-19 22:52:35 +0800

    update

--- dse/download.sh
+++ dse/download.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-WGET="wget --user=guru.lin@gmail.com --password Tb946060"
+WGET="wget --user=guru.lin@gmail.com --password password"
 
 #declare -a arr=("/datastax-studio/datastax-studio.tar.gz")
 declare -a arr=("enterprise/dse.tar.gz" "enterprise/opscenter.tar.gz" "/datastax-studio/datastax-studio.tar.gz")
