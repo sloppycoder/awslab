48cebf992b2664d12e185ce44d13f4c90ea1a180
Li Lin <guru.lin@gmail.com>
2019-03-21 19:10:10 +0800

    use Ansible playbook for DSE install

--- dse/dse.rb
+++ dse/dse.rb
@@ -5,9 +5,10 @@ require_relative '../lib/awslab'
 def instance_tags(role, fqdn)
   {
     'Project' => 't-co-dse',
-    'os' => 'Linux',
-    'Role' => role,
-    'FQDN' => fqdn
+    'os'      => 'Linux',
+    'Role'    => role,
+    'FQDN'    => fqdn,
+    'Name'    => 'dse'
   }
 end
 
@@ -15,6 +16,15 @@ def set_hostname(fqdn)
   "hostnamectl set-hostname #{fqdn}"
 end
 
+def extra_pkgs
+  %(
+yum install -y epel-release
+yum install -y htop jq
+
+)
+end
+
+#
 # isntance types
 #   m5a.large   2/ 8/0.10   vcpu/G ram/per hour usage us-west-2
 #   m5a.xlarge  4/16/0.20
@@ -27,12 +37,31 @@ iam_profile = conf[:inst_profile]
 subnet_id = conf[:private_subnet]
 keypair = conf[:keypair]
 
+=begin
+
+search for offical Centos7 image in AWS marketplace
+get product code from https://wiki.centos.org/Cloud/AWS, then search using aws cli
+
+aws ec2 describe-images \
+           --owners 'aws-marketplace' \
+           --filters 'Name=product-code,Values=aw0evgkw8e5c1q413zgy5pjce' \
+           --query 'sort_by(Images, &CreationDate)[-1].[ImageId]' \
+           --output 'text'
+
+returns
+
+ ami-01ed306a12b7d1c96
+
+=end
+
+
 ec2 = Aws::EC2::Resource.new(region: region)
 
 { 'dse' => 'dse.t.co' }.each do |role, fqdn|
   create_instances(ec2, keypair, subnet_id,
-                   instance_type: 'm5a.xlarge',
-                   iam_role_profile: iam_profile,
-                   startup_script: base_startup_script(set_hostname(fqdn)),
-                   tags: instance_tags(role, fqdn))
+       image_id: 'ami-01ed306a12b7d1c96',
+       instance_type: 'm5a.xlarge',
+       iam_role_profile: iam_profile,
+       startup_script: base_startup_script(set_hostname(fqdn) + extra_pkgs),
+       tags: instance_tags(role, fqdn))
 end
--- dse/setup_dir.sh
+++ dse/setup_dir.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-set -e 
+set -e
 
-BASE_DIR=`pwd`
+if [ "$1" = ""]; then
+	BASE_DIR=`pwd`
+else
+	BASE_DIR=$1
+fi
 
-makedir () {
-    mkdir -p $1
-    chown datastax:datastax $1
-}
+echo # settting up DSE data directories at $BASE_DIR
 
-makedir $BASE_DIR
+declare -a arr=(
+	"/var/lib/cassandra"
+	"/var/log/cassandra"
+	"/var/log/spark"
+	"/var/lib/spark"
+	"/var/lib/dsefs"
+	"/var/lib/spark/worker"
+	"/var/lib/spark/rdd"
+	)
 
-declare -a arr=("/var/lib/cassandra" "/var/log/cassandra" "/var/log/spark" "/var/lib/spark" "/var/lib/dsefs" "/var/lib/spark/worker" "/var/lib/spark/rdd")
 for dir in "${arr[@]}"
 do
-    makedir $BASE_DIR$dir
+	if [ -d "$BASE_DIR$dir" ]; then
+		rm -rf $BASE_DIR$dir
+	fi
+
+    mkdir -p $BASE_DIR$dir
+
+    # needs root to make link from /var
     echo sudo ln -s $BASE_DIR$dir $dir
 done
