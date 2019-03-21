d41ce28389bf82859cb815802c23154bdd9de45f
Li Lin <guru.lin@gmail.com>
2019-03-21 16:28:24 +0000

    add helper scripts for install sdkman and polish

---
+++ dse/setup_sdkman.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+umask 0022
+
+if [ ! -f $HOME/.bash_profile ]; then
+
+    cat << EOF > $HOME/.bash_profile
+if [ -f ~/.bashrc ]; then
+        . ~/.bashrc
+fi
+EOF
+
+fi
+
+touch $HOME/.bashrc
+
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install maven 3.3.9
+sdk install gradle 4.10.3
+sdk install sbt 1.2.8
+
